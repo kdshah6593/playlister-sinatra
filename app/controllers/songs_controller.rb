5783b37dcba3c53d9519a914a2087f01d671c3f6
Kunal <kdshah6593@gmail.com>
2020-12-25 13:06:18 -0500

    commit tree f05e075bdfa8b2ec7bb9a947a56fca5aff841f4a

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,41 +3,41 @@ require 'rack-flash'
 class SongsController < ApplicationController
     use Rack::Flash
 
-    #Present the user with list of all songs; each song clickable link
-    #index
     get '/songs' do
         erb :'songs/index'
     end
 
-    #new song
     get '/songs/new' do
         erb :'songs/new'
     end
 
     post '/songs' do
         @song = Song.create(params[:song])
-        #REMEBER THE FIND OR CREATE BY METHOD
-        if Artist.find_by(name: params[:artist][:name])
-            @song.artist = Artist.find_by(name: params[:artist][:name])
-        else
-            @artist = Artist.create(params[:artist])
-        end
+        @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
         @song.genre_ids = params[:genres]
         @song.save
-        
-        redirect to "/songs/#{@song.slug}"
+        flash[:message] = "Successfully created song."
+        redirect "/songs/#{@song.slug}"
     end
 
-    #Any given song's show page should have links to that song's artist and each genre associated with the song | this is instead of ID
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         erb :'songs/show'
     end
 
     get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
         erb :'songs/edit'
     end
 
     patch '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        @song.update(params[:song])
+        @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        @song.genre_ids = params[:genres]
+        @song.save
+
+        flash[:message] = "Successfully updated song."
+        redirect "/songs/#{@song.slug}"
     end
 end
